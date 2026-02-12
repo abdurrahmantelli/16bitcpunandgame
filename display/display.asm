79bc2dcfbb24c94930a303b52fc0aae1a819a21a
abdurrahmantelli <telliabdurrahman@gmail.com>
2026-02-12 14:35:44 +0300

    Add files via upload

---
+++ display/display.asm
@@ -0,0 +1,96 @@
+# SATIR 1
+A = 0x0180
+D = A
+A = 0x4000
+*A = D
+
+# SATIR 2
+A = 0x03C0
+D = A
+A = 0x4020
+*A = D
+
+# SATIR 3
+A = 0x07E0
+D = A
+A = 0x4040
+*A = D
+
+# SATIR 4
+A = 0x0FF0
+D = A
+A = 0x4060
+*A = D
+
+# SATIR 5
+A = 0x1FF8
+D = A
+A = 0x4080
+*A = D
+
+# SATIR 6
+A = 0x3FFC
+D = A
+A = 0x40A0
+*A = D
+
+# SATIR 7
+A = 0x7FFE
+D = A
+A = 0x40C0
+*A = D
+
+# SATIR 8 (Tam 16 piksel genişlik için 0xFFFF üretimi)
+A = 0
+D = ~A
+A = 0x40E0
+*A = D
+
+# SATIR 9 (Tam 16 piksel genişlik için 0xFFFF üretimi)
+A = 0
+D = ~A
+A = 0x4100
+*A = D
+
+# SATIR 10
+A = 0x7FFE
+D = A
+A = 0x4120
+*A = D
+
+# SATIR 11
+A = 0x3FFC
+D = A
+A = 0x4140
+*A = D
+
+# SATIR 12
+A = 0x1FF8
+D = A
+A = 0x4160
+*A = D
+
+# SATIR 13
+A = 0x0FF0
+D = A
+A = 0x4180
+*A = D
+
+# SATIR 14
+A = 0x07E0
+D = A
+A = 0x41A0
+*A = D
+
+# SATIR 15
+A = 0x03C0
+D = A
+A = 0x41C0
+*A = D
+
+# SATIR 16
+A = 0x0180
+D = A
+A = 0x41E0
+*A = D
+
